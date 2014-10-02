56db1a27af9b7e665bea7ca0ae44999afb7c4363
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-01 22:48:40 -0400

    Creating true and false buttons, but with no implementations yet

--- app/src/main/java/com/detroitlabs/kyleofori/geoquiz/QuizActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/geoquiz/QuizActivity.java
@@ -4,14 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Between the parentheses is an anonymous class that implements the listener
+                //It will need to implement the onClick(View) method for OnClickListener, too
+            }
+        });
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Same situation
+            }
+        });
     }
 
 
