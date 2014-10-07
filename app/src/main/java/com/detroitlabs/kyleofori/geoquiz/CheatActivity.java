6c7c596663b246d501499ade39bda1292f613771
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-07 09:12:17 -0400

    Enabling cheating

--- app/src/main/java/com/detroitlabs/kyleofori/geoquiz/CheatActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/geoquiz/CheatActivity.java
@@ -2,6 +2,9 @@ package com.detroitlabs.kyleofori.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by kyleofori on 10/6/14.
@@ -10,7 +13,10 @@ public class CheatActivity extends Activity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
 
-    private boolean mAnswerIsTrue;x
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -19,5 +25,22 @@ public class CheatActivity extends Activity {
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mAnswerIsTrue)
+                {
+                    mAnswerTextView.setText(R.string.true_button);
+                }
+                else
+                {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
+    
 }
