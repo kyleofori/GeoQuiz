6f07e12d5652062d08c3112788e89be7d1dc9046
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-06 22:42:33 -0400

    Overriding onCreate()

---
+++ app/src/main/java/com/detroitlabs/kyleofori/geoquiz/CheatActivity.java
@@ -0,0 +1,17 @@
+package com.detroitlabs.kyleofori.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kyleofori on 10/6/14.
+ */
+public class CheatActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+    }
+}
